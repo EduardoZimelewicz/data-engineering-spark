77d5105159f77fcaf5000feca58f35f85873c3b4
EduardoZimelewicz <eduardo.zimelewicz@gmail.com>
2022-03-02 19:06:15 -0300

    section 10 - lesson 104 finished

--- init-itversity-env.sh
+++ init-itversity-env.sh
@@ -7,3 +7,10 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     GRANT ALL ON DATABASE itversity_sms_db TO itversity_sms_user;
     GRANT pg_read_server_files TO itversity_sms_user;
 EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE itversity_retail_db;
+    CREATE USER itversity_retail_user WITH ENCRYPTED PASSWORD 'retail_password';
+    GRANT ALL ON DATABASE itversity_retail_db TO itversity_retail_user;
+    GRANT pg_read_server_files TO itversity_retail_user;
+EOSQL
